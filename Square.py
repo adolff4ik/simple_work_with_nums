444e49deed08dfad35e2525f982c81d0e88276a6
Serhiy Ivashchenko <109664845+adolff4ik@users.noreply.github.com>
2023-03-21 22:02:24 +0200

    Update Square.py

--- Square.py
+++ Square.py
@@ -1,13 +1,11 @@
-from re import S, X
-
-
 kub = input('Введіть сторону:')
 
 def kBadrat ():
     S = int(kub)*int(kub)
     P = int(kub)*4
     X = float(2 ** .5)*int(kub)
-    print(S,P,X)
-    return [S, P, X]
+    print('S =', S)
+    print('P =', P)
+    print('X =', X)
 
 kBadrat ()
