4ca57de1fd7e8bd7ab4e7e8838368cb9387ecb8e
Serhiy Ivashchenko <109664845+adolff4ik@users.noreply.github.com>
2023-03-21 21:55:22 +0200

    Update Simple numbers.py

--- Simple numbers.py
+++ Simple numbers.py
@@ -1,12 +1,12 @@
 import math
 
-num = input('Введіть число від 0 до 1000:')
+num1 = input('Введіть число від 0 до 1000:')
 
-num2 = math.trunc(float(num))
+num2 = math.trunc(float(num1))
 
-def is_prime (num):
-    if float(num) == int(num2):
+def is_prime (num1):
+    if float(num1) == int(num2):
         return True
     else: return False
 
-print(is_prime (num))
+print(is_prime (num1))
